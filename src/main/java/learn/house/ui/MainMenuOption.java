8f2505afc307f7740b463f93f8e4525b4a0f6179
cole99 <67916002+ColeAbell@users.noreply.github.com>
2022-07-17 21:55:36 -0400

    Main Menu adjust text

--- src/main/java/learn/house/ui/MainMenuOption.java
+++ src/main/java/learn/house/ui/MainMenuOption.java
@@ -2,27 +2,27 @@ package learn.house.ui;
 
 public enum MainMenuOption {
 
-    ADD_GUEST (1, "Add A Guest Profile"),
+    ADD_GUEST (1, "Add Guest Profile"),
 
     UPDATE_GUEST (2, "Update Guest Profile"),
 
-    VIEW_GUEST (3, "View A Guest's Profile"),
+    VIEW_GUEST (3, "View Guest Profile / Reservations"),
 
-    DELETE_GUEST (4, "Delete A Guest Profile"),
+    DELETE_GUEST (4, "Delete Guest Profile"),
 
-    ADD_HOST (5, "Add A Host Profile"),
+    ADD_HOST (5, "Add Host Profile"),
 
-    UPDATE_HOST (6, "Update A Host Profile"),
+    UPDATE_HOST (6, "Update Host Profile"),
 
-    VIEW_HOST (7, "View A Host Profile / Reservations"),
+    VIEW_HOST (7, "View Host Profile / Reservations"),
 
-    DELETE_HOST (8, "Delete A Host Profile"),
+    DELETE_HOST (8, "Delete Host Profile"),
 
-    MAKE_RESERVATION (9, "Make A Reservation"),
+    MAKE_RESERVATION (9, "Make Reservation"),
 
-    UPDATE_RESERVATION (10, "Edit A Reservation"),
+    UPDATE_RESERVATION (10, "Edit Reservation"),
 
-    DELETE_RESERVATION (11, "Cancel A Reservation"),
+    DELETE_RESERVATION (11, "Cancel Reservation"),
 
     EXIT (12, "Exit");
 
